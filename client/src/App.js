55be06e6c64c76160eb4400de73018f74d832ebf
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-04-12 18:32:20 -0400

    basic framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import "./reset.css"
 import "./App.css";
+import MainView from './MainView';
+import Header from './Header';
+import Sidebar from './Sidebar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Header />
+        <div className="wrapper">
+          <Sidebar />
+          <MainView />
+        </div>
       </div>
     );
   }
