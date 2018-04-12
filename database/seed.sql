c5291d627980bfb130fb438b7777a50a840bacca
BDMartinez <BDMartinez@Live.com>
2018-04-12 18:28:19 -0400

    Began hardcoding entries into the the seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,150 @@
+
+
+-- USERS --
+INSERT INTO users
+  (username, id, password_digest, cards)
+VALUES
+  ("Jack", 1, );
+
+INSERT INTO users
+  (username, id, password_digest, cards)
+VALUES
+  ("Jill", 2, );
+
+INSERT INTO users
+  (username, id, password_digest, cards)
+VALUES
+  ("Meh", 3, );
+
+
+--------------------------------------------------------------------------------
+
+-- DECKS --
+INSERT INTO Decks
+(id, title, user_id)
+VALUES
+  (1, "Vanilla JS", 1);
+
+INSERT INTO Decks
+  (id, title, user_id)
+VALUES
+  (2, "", 1);
+
+INSERT INTO Decks
+  (id, title, user_id)
+VALUES
+  (3, "SQL", 2);
+
+INSERT INTO Decks
+  (id, title, user_id)
+VALUES
+  (4, "High Order Functions", 2);
+
+INSERT INTO Decks
+  (id, title, user_id)
+VALUES
+  (5, "React", 3);
+
+INSERT INTO Decks
+  (id, title, user_id)
+VALUES
+  (6, "React Router", 3);
+
+--------------------------------------------------------------------------------
+
+-- CARDS --
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (1, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (2, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (3, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (4, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (5, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (6, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (7, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (8, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (9, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+  VALUES
+  (10, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (11, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (12, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (13, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (14, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (15, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (16, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (17, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (18, "", "", );
+
+INSERT INTO cards
+  (id, question, answer, deck_id)
+VALUES
+  (19, "", "", );
