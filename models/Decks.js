f000cd792b7aa684cd0069986943bd3ac306b56d
Alexander Schelchere <ascellie@gmail.com>
2018-04-12 14:05:40 -0400

    The models, Decks and Cards, ladies and gents!

--- models/Decks.js
+++ models/Decks.js
@@ -2,4 +2,30 @@ const db = require("../database/db-connection");
 
 const Decks = {};
 
+Decks.getOne = slug => {
+  return db.one("SELECT * FROM decks WHERE slug = $1", [slug]);
+};
+
+Decks.getUserDecks = user_id => {
+  return db.any("SELECT * FROM decks WHERE user_id = $1", [user_id]);
+};
+
+Decks.getPublicDecks = data => {
+  return db.any("SELECT * FROM decks WHERE user_id <> $1 AND public = true", [
+    data.user_id,
+    data.public
+  ]);
+};
+
+Decks.update = deckData => {
+  return db.none(
+    "UPDATE decks SET title = $1, slug = $2, public = $3 WHERE id = $4",
+    [deckData.title, deckData.slug, deckData.public, deckData.id]
+  );
+};
+
+Decks.delete = id => {
+  return db.result("DELETE FROM decks WHERE id = $1", [id]);
+};
+
 module.exports = Decks;
