ece04c175ff2a6516de26ecea86ef0eb9e0b8fe7
Alexander Schelchere <ascellie@gmail.com>
2018-04-13 14:03:30 -0400

    review is now GamePlay Normal is now GameNormal Kaizen is now GameTailored.

---
+++ client/src/GamePlay/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class GamePlay extends Component {}
+
+export default GamePlay;
---
+++ client/src/GameTailored/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class GameTailored extends Component {}
+
+export default GameTailored;
