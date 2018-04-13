1889a30397a30b43910ec34e51828ff4816a196a
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-04-13 11:32:32 -0400

    Style updates

--- client/src/Header/index.js
+++ client/src/Header/index.js
@@ -6,15 +6,26 @@ const Header = (props) => {
 // QUESTION: we want the currently selected view to be highlighted.
 // can this come from the react router match props?
     return (
-      <div className="header">
-        <h2 className="nav-item dashboard">Dashboard</h2>
-        <h2 className="nav-item create">Create Deck</h2>
-        <h2 className="nav-item your-decks">Your Decks</h2>
-        <h2 className="nav-item review">Review</h2>
-        <div className="header-stretch"></div>
-        <h2 className="nav-item user-icon">
-          <img src={UserImg} alt="user" />
-        </h2>
+      <div>
+        {/* Mobile view */}
+        <div className="header header-mobile">
+          <h2 className="nav-item dashboard">Dashboard</h2>
+          <div className="header-stretch"></div>
+          <h2 className="nav-item user-icon">
+            <img src={UserImg} alt="user" />
+          </h2>
+        </div>
+        {/* Desktop View */}
+        <div className="header header-desktop">
+          <h2 className="nav-item dashboard">Dashboard</h2>
+          <h2 className="nav-item create">Create Deck</h2>
+          <h2 className="nav-item your-decks">Your Decks</h2>
+          <h2 className="nav-item review">Review</h2>
+          <div className="header-stretch"></div>
+          <h2 className="nav-item user-icon">
+            <img src={UserImg} alt="user" />
+          </h2>
+        </div>
       </div>
     );
 }
