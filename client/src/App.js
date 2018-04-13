1e5d296ec04619f671d0e6a8210de2fd771f478f
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-04-13 13:32:25 -0400

    Moved main view components to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import "./reset.css";
 import "./App.css";
 import Header from "./Header";
-import UserDecks from "./UserDecks";
-import SavedDecks from "./SavedDecks";
-import PublicDecks from "./PublicDecks";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
   render() {
@@ -12,24 +10,7 @@ class App extends Component {
       <div className="app">
         <Header />
         <div className="main-view">
-          {/* Route: dashboard */}
-            <div className="deck-section">
-              <h2>Your Decks</h2>
-              <UserDecks />
-            </div>
-
-            {/* if no saved decks, hide this section */}
-              <div className="deck-section">
-                <h2>Saved Decks</h2>
-                <SavedDecks />
-              </div>
-            {/* end of saved decks ternary */}
-
-            <div className="deck-section">
-              <h2>Public Decks</h2>
-              <PublicDecks />
-            </div>
-          {/* End of dashboard route */}
+          <Dashboard />
         </div>
       </div>
     );
