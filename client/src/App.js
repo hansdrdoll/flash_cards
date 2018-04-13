bb50663503ba293404f9ce03a00d7b70f0f67f28
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-04-13 10:37:31 -0400

    Nuked mainview component yay simplicity

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import "./reset.css"
 import "./App.css";
-import MainView from './MainView';
 import Header from './Header';
-import Sidebar from './Sidebar';
+import UserDecks from "./UserDecks";
+import SavedDecks from "./SavedDecks";
+import PublicDecks from "./PublicDecks";
+
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <MainView />
+        <div className="main-view">
+          <div className="deck-section">
+            <UserDecks />
+          </div>
+          <div className="deck-section">
+            <SavedDecks />
+          </div>
+          <div className="deck-section">
+            <PublicDecks />
+          </div>
+        </div>
       </div>
     );
   }
