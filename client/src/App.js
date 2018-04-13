2682de1ed04098869b550896223ceb61e26f619d
Alexander Schelchere <ascellie@gmail.com>
2018-04-13 15:50:29 -0400

    routes functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './reset.css';
-import './App.css';
-import Header from './Header';
-import Dashboard from './Dashboard';
-import CreateDeck from './CreateDeck';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./reset.css";
+import "./App.css";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+import CreateDeck from "./CreateDeck";
+import GamePlay from "./GamePlay";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Header />
         <Router>
-          <div className="main-view">
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/decks/new" component={CreateDeck} />
-            {/* <Route exact path="/" component={} /> */}
+          <div className="all">
+            <Header />
+            <div className="main-view">
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/play" component={GamePlay} />
+              <Route exact path="/decks/new" component={CreateDeck} />
+              {/* <Route exact path="/" component={} /> */}
+            </div>
           </div>
         </Router>
       </div>
