ca5d44979b69518432579603000d1104dc02bd55
Alexander Schelchere <ascellie@gmail.com>
2018-04-14 16:25:29 -0400

    hope this ain't broken

--- client/src/UserDeck/index.js
+++ client/src/UserDeck/index.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { Card, Button } from 'semantic-ui-react';
+import { Card, Button } from "semantic-ui-react";
 
 const UserDeck = props => {
   const { deckItem } = props;
   return (
     <Card>
-    <Card.Content header={deckItem.title} />
-    {/* <Card.Content description={"deck number " + props.deckItem.id} /> */}
-    <Card.Content extra>
-      {/* <Button.Group> */}
-        <Button floated='left'>Review</Button>
-        <Button floated='right'>Edit</Button>
-      {/* </Button.Group> */}
-    </Card.Content>
-  </Card>
-
+      <Card.Content header={deckItem.title} />
+      {/* <Card.Content description={"deck number " + props.deckItem.id} /> */}
+      <Card.Content extra>
+        {/* <Button.Group> */}
+        <Button floated="left">Review</Button>
+        <Button floated="right">Edit</Button>
+        {/* </Button.Group> */}
+      </Card.Content>
+    </Card>
   );
 };
 
